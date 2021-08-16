ef4fce99f858ec6430173e31ea2b8c101a868710
seongbiny <s30ngb1n@gmail.com>
2021-08-16 18:42:16 +0900

    210816

--- SWEA/1961.py
+++ SWEA/1961.py
@@ -1,25 +1,39 @@
 T = int(input())
-for tc in range(1,T+1):
-    N = int(input())
-    arr = [list(map(int,input().split())) for _ in range(N)]
-    # 2차배열 받음
+for tc in range(1, T + 1):
+    n = int(input())
+    arr = [list(map(str, input().split())) for _ in range(n)]
+    # print(arr)
+    arr_90 = [[0] * n for _ in range(n)]
+    arr_180 = [[0] * n for _ in range(n)]
+    arr_270 = [[0] * n for _ in range(n)]
+    # 90 도 회전
+    # 00 - 02 / 01 - 12 / 02 - 22
+    # 10 - 01 / 11 - 11 / 12 - 21
+    # 20 - 00 / 21 - 10 / 22 - 20
+    for i in range(len(arr)):
+        for j in range(n):
+            arr_90[j][n - 1 - i] = arr[i][j]
+    # print(arr_90)
 
-    #def turn_90():
-    newarr = []
-    dx = [2, 1, 0, 1, 1, 0, -1, 2]
-    dy = [0, -1, -2, 1, -1, 2, 1, 0]
-    '''
-    0: arr[i+dx[0]][i+dy[0]]
-    1: arr[i+dx[1]][i+dy[1]]
-    2: arr[i+dx[2]][i+dy[2]]
-    ...
-    '''
-    for i in range(N): # 0 1 2
-        for j in range(N): # 0 1 2
-         # 0 1 2 3 4 5 6 7
-            newi = i +dx[j] # 0+0
-            newj = j +dy[j] # 0+0
-            if 0 <= newi < N and 0 <= newj < N:
-                print(arr[newi][newj])
+    for i in range(len(arr_90)):
+        for j in range(n):
+            arr_180[j][n - 1 - i] = arr_90[i][j]
+    # print(arr_180)
 
-    #print(newarr)
+    for i in range(len(arr_180)):
+        for j in range(n):
+            arr_270[j][n - 1 - i] = arr_180[i][j]
+    # print(arr_270)
+
+    # arr_90[0], arr_90[1], arr_90[2] 안에 원소들 다 붙이고 00 10 20 으로
+    # arr_180[0], arr_180[1], arr_180[2] 안에 원소들 다 붙이고 01 11 21
+    # arr_270[0], arr_270[1], arr_270[2] 안에 원소들 다 붙이고 02 12 22
+    new_90 = 0
+    new_180 = 0
+    new_270 = 0
+    print(f'#{tc} ')
+    for i in range(len(arr)):
+        new_90 = ''.join(arr_90[i])
+        new_180 = ''.join(arr_180[i])
+        new_270 = ''.join(arr_270[i])
+        print(new_90, new_180, new_270)
