e501007598f49fc9f8993d8648b55016c8ff11c1
seongbiny <s30ngb1n@gmail.com>
2021-10-10 20:03:41 +0900

    211010

--- SWEA/1244최대상금.py
+++ SWEA/1244최대상금.py
@@ -1,37 +1,24 @@
+def swap(a):
+    global cnt
+    cur_nums = int("".join(nums))
+    if a == change:
+        cnt = max(cnt, cur_nums)
+        return
+
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            nums[i], nums[j] = nums[j], nums[i]
+            swap(a+1)
+            nums[i], nums[j] = nums[j], nums[i]
+
+
 T = int(input())
 for tc in range(1, T+1):
-    num, cnt = input().split()
-    cnt = int(cnt)
-    nums = []
-    for i in range(len(num)):
-        nums.append(int(num[i]))
-    min_nums = nums[:]
-    max_nums = nums[:]
-    minmin = nums[:]
-    maxmax = nums[:]
-    min_index = []
-    max_index = []
-    min_nums.sort()
-    max_nums.sort(reverse=True)
+    nums, change = input().split()
+    nums = list(nums)
 
-    for i in min_nums:
-        min_index.append(minmin.index(i))
-        minmin[minmin.index(i)] = 0
+    change = int(change)
+    cnt = 0
 
-    for i in max_nums: # 7 7 3 2
-        for j in range(len(maxmax)-1, -1, -1): # 3 2 1 0
-            if maxmax[j] == i:
-                max_index.append(j)
-                maxmax[j] = 0
-
-    while cnt > 0:
-
-
-        cnt -= 1
-
-    for i in range(len(nums)):
-        nums[i] = str(nums[i])
-    result = ''.join(nums)
-    print(f'#{tc} {result}')
-
-
+    swap(0)
+    print(f'#{tc} {cnt}')
--- SWEA/5186.py
+++ SWEA/5186.py
@@ -1,15 +1,13 @@
 T = int(input())
-
 for tc in range(1, T+1):
     N = float(input())
     result = ''
-    for i in range(-1, -14, -1):
-        result += str(int(N // (2**i)))
-        N %= (2**i)
-        if N == 0:
+    for i in range(1, 13):
+        N *= 2
+        result += str(int(N)%2)
+        if N % 1 == 0:
             break
     else:
+        # 12번 동안 안되면
         result = 'overflow'
-
     print(f'#{tc} {result}')
-
