e141c3b62831cccf0cb664ad34b1a82e1f9ae56d
seongbiny <s30ngb1n@gmail.com>
2021-10-11 03:30:50 +0900

    211011

--- SWEA/5208.py
+++ SWEA/5208.py
@@ -1,20 +1,19 @@
-def dfs(i, max_i, cnt):
-    global res
-    if cnt >= res:
-        return
-    if max_i >= N:
-        res = min(cnt, res)
-        return
-    for j in range(max_i, i, -1):
-        dfs(j, j + M[j], cnt + 1)
-
+def move(idx, e, c):
+    global ans
+    if idx == bus_stop[0]:
+        if ans > c:
+            ans = c
+    else:
+        # 배터리를 교체하지 않고 내려보내기
+        if e > 0:
+            move(idx+1, e-1, c)
+        # 배터리를 교체하고 내려보내기
+        if c < ans:
+            move(idx+1, bus_stop[idx]-1, c+1)
 
 T = int(input())
 for tc in range(1, T+1):
-    tmp = list(map(int, input().split()))
-    N = tmp[0]
-    M = [0] + tmp[1:]
-
-    res = 10000
-    dfs(1, 1+M[1], 0)
-    print(f'#{tc} {res}')
+    bus_stop = list(map(int, input().split()))
+    ans = 9999999
+    move(2, bus_stop[1]-1, 0)
+    print(f'#{tc} {ans}')
