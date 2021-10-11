011c581f17b6580c6022298ec9df7d219f2d44ad
seongbiny <s30ngb1n@gmail.com>
2021-10-12 00:48:17 +0900

    211011

--- SWEA/1861.py
+++ SWEA/1861.py
@@ -1,34 +1,33 @@
-# 어차피 다음 방 번호가 현재 방 번호+1 이어야 움직일 수 있다는 조건이 있으므로
-# 중복되는 경로가 없다. => visited 필요없음
-
-def dfs(room[x][y], idx):
-    global cnt
-    if idx < cnt:
-        return
-
-    if x == N-1 and y == N-1:
-
-        return
-
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-        if 0 <= nx < N and 0 <= ny < N:
-            if room[x][y] + 1 == room[nx][ny]:
-                idx += 1
-                dfs(room[nx][ny], idx)
-
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     room = [list(map(int, input().split())) for _ in range(N)]
 
-    cnt = 0
-    dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
+    dx = [1, 0, 0, -1]
+    dy = [0, 1, -1, 0]
+    check = N*N*[0]+[0]
 
-    for x in range(N):
-        for y in range(N):
-            dfs(room[x][y], idx)
-
-    print(f'#{tc} {room[x][y]} {idx}')
+    for i in range(N):
+        for j in range(N):
+            for d in range(4):
+                start = room[i][j]
+                nx = i + dx[d]
+                ny = j + dy[d]
+                if 0 <= nx < N and 0 <= ny < N:
+                    if room[nx][ny] == start + 1:
+                        check[start] += 1
+    #print(check)
+    i = N
+    cnt = 1
+    max_min = 0
+    idx = 0
+    while i >= 0:
+        if check[i] == 1:
+            cnt += 1
+        else:
+            if max_min <= cnt:
+                max_min = cnt
+                idx = i
+            cnt = 1
+        i -= 1
+    print(f'#{tc} {idx+1} {max_min}')
