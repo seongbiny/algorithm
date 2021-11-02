403e1ffd1a832332ac2861563790d28bafa6ee83
seongbiny <s30ngb1n@gmail.com>
2021-11-03 01:37:12 +0900

    211102

--- SWEA/1227.py
+++ SWEA/1227.py
@@ -1,42 +1,54 @@
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-def solve(x, y):
-    global cnt, end_x, end_y
-    visited[x][y] = 1
-    if x == end_x and y == end_y:
-        cnt = 1
-        return
+# def dfs(x, y):
+#     global result
+#
+#     if arr[x][y] == 3:
+#         result = 1
+#         return
+#
+#     for d in range(4):
+#         nx = x + dx[d]
+#         ny = y + dy[d]
+#         if 0 <= nx < 100 and 0 <= ny < 100:
+#             if visited[nx][ny] == 1 or arr[nx][ny] == 1:
+#                 continue
+#             else:
+#                 visited[nx][ny] = 1
+#                 dfs(nx, ny)
 
-    for d in range(4):
-        new_x = x + dx[d]
-        new_y = y + dy[d]
+def bfs(x, y):
+    global result
+    q = [(si, sj)]
+    arr[si][sj] = 1
 
-        if 0 <= new_x < 100 and 0 <= new_y < 100:
-            if not visited[new_x][new_y] and arr[new_x][new_y] != 1:
-                visited[new_x][new_y] = 1
-                if arr[new_x][new_y] == 3:
-                    cnt = 1
-                solve(new_x, new_y)
+    while q:
+        x, y = q.pop(0)
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if arr[nx][ny] == 0:
+                q.append((nx, ny))
+                arr[nx][ny] = 1
+            elif arr[nx][ny] == 1:
+                continue
+            elif arr[nx][ny] == 3:
+                result = 1
+                return
 
 
-for tc in range(1):
+for _ in range(10):
+    tc = input()
     arr = [list(map(int, input())) for _ in range(100)]
-    visited = [[0] * 100 for _ in range(100)]
-    cnt = 0
+    visited = [[0]*100 for _ in range(100)]
+    si, sj = 0, 0
+    for i in range(100):
+        for j in range(100):
+            if arr[i][j] == 2:
+                si, sj = i, j
 
-    for x in range(100):
-        for y in range(100):
-            if arr[x][y] == 2:
-                start_x = x
-                start_y = y
-            elif arr[x][y] == 3:
-                end_x = x
-                end_y = y
-
-    solve(start_x, start_y)
-    print(f'#{tc} {cnt}')
-
-
-
-
+    result = 0
+    # dfs(si, sj)
+    bfs(si, sj)
+    print(f'#{tc} {result}')
